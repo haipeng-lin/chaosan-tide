7e1a9e90068c7d7d191496c5ed8004e03f68a85f
haipeng-lin <haipeng_lin@163.com>
2024-11-24 15:27:19 +0800

    update：后台文化专题、标签页面优化

--- tide-admin/src/views/cultural/culturalTopicContent/index.vue
+++ tide-admin/src/views/cultural/culturalTopicContent/index.vue
@@ -1,172 +1,192 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="文化专题id" prop="culturalTopicId">
-        <el-input
-          v-model="queryParams.culturalTopicId"
-          placeholder="请输入文化专题id"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="标签id1" prop="tagId1">
-        <el-input
-          v-model="queryParams.tagId1"
-          placeholder="请输入标签id1"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="标签id2" prop="tagId2">
-        <el-input
-          v-model="queryParams.tagId2"
-          placeholder="请输入标签id2"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="标签id3" prop="tagId3">
-        <el-input
-          v-model="queryParams.tagId3"
-          placeholder="请输入标签id3"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="封面" prop="coverImg">
-        <el-input
-          v-model="queryParams.coverImg"
-          placeholder="请输入封面"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="1-置顶，0-不置顶" prop="isTop">
-        <el-input
-          v-model="queryParams.isTop"
-          placeholder="请输入1-置顶，0-不置顶"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="1-推荐，0-不推荐" prop="isRecommend">
-        <el-input
-          v-model="queryParams.isRecommend"
-          placeholder="请输入1-推荐，0-不推荐"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+    <!-- 搜索文本框 -->
+    <el-form :model="queryParams"
+             ref="queryRef"
+             :inline="true"
+             v-show="showSearch"
+             label-width="68px">
+      <el-form-item label="文化专题"
+                    prop="culturalTopicId">
+        <el-input v-model="queryParams.culturalTopicId"
+                  placeholder="请输入文化专题"
+                  clearable
+                  @keyup.enter="handleQuery" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
-        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+        <el-button type="primary"
+                   icon="Search"
+                   @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh"
+                   @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
 
-    <el-row :gutter="10" class="mb8">
+    <!-- 增删查改按钮 -->
+    <el-row :gutter="10"
+            class="mb8">
       <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['cultural:culturalTopicContent:add']"
-        >新增</el-button>
+        <el-button type="primary"
+                   plain
+                   icon="Plus"
+                   @click="handleAdd"
+                   v-hasPermi="['cultural:culturalTopicContent:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['cultural:culturalTopicContent:edit']"
-        >修改</el-button>
+        <el-button type="success"
+                   plain
+                   icon="Edit"
+                   :disabled="single"
+                   @click="handleUpdate"
+                   v-hasPermi="['cultural:culturalTopicContent:edit']">修改</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['cultural:culturalTopicContent:remove']"
-        >删除</el-button>
+        <el-button type="danger"
+                   plain
+                   icon="Delete"
+                   :disabled="multiple"
+                   @click="handleDelete"
+                   v-hasPermi="['cultural:culturalTopicContent:remove']">删除</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['cultural:culturalTopicContent:export']"
-        >导出</el-button>
+        <el-button type="warning"
+                   plain
+                   icon="Download"
+                   @click="handleExport"
+                   v-hasPermi="['cultural:culturalTopicContent:export']">导出</el-button>
       </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar v-model:showSearch="showSearch"
+                     @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="culturalTopicContentList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="id" align="center" prop="id" />
-      <el-table-column label="文化专题id" align="center" prop="culturalTopicId" />
-      <el-table-column label="标签id1" align="center" prop="tagId1" />
-      <el-table-column label="标签id2" align="center" prop="tagId2" />
-      <el-table-column label="标签id3" align="center" prop="tagId3" />
-      <el-table-column label="封面" align="center" prop="coverImg" />
-      <el-table-column label="内容" align="center" prop="content" />
-      <el-table-column label="1-置顶，0-不置顶" align="center" prop="isTop" />
-      <el-table-column label="1-推荐，0-不推荐" align="center" prop="isRecommend" />
-      <el-table-column label="显示状态" align="center" prop="status" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+    <!-- 数据 -->
+    <el-table v-loading="loading"
+              :data="culturalTopicContentList"
+              @selection-change="handleSelectionChange">
+      <el-table-column type="selection"
+                       width="55"
+                       align="center" />
+      <el-table-column label="id"
+                       align="center"
+                       prop="id" />
+      <el-table-column label="文化专题id"
+                       align="center"
+                       prop="culturalTopicId" />
+      <el-table-column label="标签id1"
+                       align="center"
+                       prop="tagId1" />
+      <el-table-column label="标签id2"
+                       align="center"
+                       prop="tagId2" />
+      <el-table-column label="标签id3"
+                       align="center"
+                       prop="tagId3" />
+      <el-table-column label="封面"
+                       align="center"
+                       prop="coverImg" />
+      <el-table-column label="内容"
+                       align="center"
+                       prop="content" />
+      <el-table-column label="置顶状态"
+                       align="center"
+                       prop="isTop">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['cultural:culturalTopicContent:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['cultural:culturalTopicContent:remove']">删除</el-button>
+          <el-switch v-model="scope.row.isTop"
+                     :active-value="1"
+                     :inactive-value="0"
+                     @change="handleSwitchChange(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="推荐状态"
+                       align="center"
+                       prop="isRecommend">
+        <template #default="scope">
+          <el-switch v-model="scope.row.isRecommend"
+                     :active-value="1"
+                     :inactive-value="0"
+                     @change="handleSwitchChange(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="显示状态"
+                       align="center"
+                       prop="status">
+        <template #default="scope">
+          <el-switch v-model="scope.row.status"
+                     :active-value="1"
+                     :inactive-value="2"
+                     @change="handleSwitchChange(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作"
+                       align="center"
+                       class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link
+                     type="primary"
+                     icon="Edit"
+                     @click="handleUpdate(scope.row)"
+                     v-hasPermi="['cultural:culturalTopicContent:edit']">修改</el-button>
+          <el-button link
+                     type="primary"
+                     icon="Delete"
+                     @click="handleDelete(scope.row)"
+                     v-hasPermi="['cultural:culturalTopicContent:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
-    <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
-    />
+
+    <pagination v-show="total>0"
+                :total="total"
+                v-model:page="queryParams.pageNum"
+                v-model:limit="queryParams.pageSize"
+                @pagination="getList" />
 
     <!-- 添加或修改文化专题内容对话框 -->
-    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
-      <el-form ref="culturalTopicContentRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="文化专题id" prop="culturalTopicId">
-          <el-input v-model="form.culturalTopicId" placeholder="请输入文化专题id" />
-        </el-form-item>
-        <el-form-item label="标签id1" prop="tagId1">
-          <el-input v-model="form.tagId1" placeholder="请输入标签id1" />
-        </el-form-item>
-        <el-form-item label="标签id2" prop="tagId2">
-          <el-input v-model="form.tagId2" placeholder="请输入标签id2" />
-        </el-form-item>
-        <el-form-item label="标签id3" prop="tagId3">
-          <el-input v-model="form.tagId3" placeholder="请输入标签id3" />
-        </el-form-item>
-        <el-form-item label="封面" prop="coverImg">
-          <el-input v-model="form.coverImg" placeholder="请输入封面" />
+    <el-dialog :title="title"
+               v-model="open"
+               width="500px"
+               append-to-body>
+      <el-form ref="culturalTopicContentRef"
+               :model="form"
+               :rules="rules"
+               label-width="80px">
+        <el-form-item label="文化专题"
+                      prop="culturalTopicId">
+          <el-input v-model="form.culturalTopicId"
+                    placeholder="请选择文化专题" />
         </el-form-item>
         <el-form-item label="内容">
-          <editor v-model="form.content" :min-height="192"/>
+          <editor v-model="form.content"
+                  :min-height="192" />
         </el-form-item>
-        <el-form-item label="1-置顶，0-不置顶" prop="isTop">
-          <el-input v-model="form.isTop" placeholder="请输入1-置顶，0-不置顶" />
+        <el-form-item label="封面"
+                      prop="coverImg">
+          <el-input v-model="form.coverImg"
+                    placeholder="请输入封面" />
         </el-form-item>
-        <el-form-item label="1-推荐，0-不推荐" prop="isRecommend">
-          <el-input v-model="form.isRecommend" placeholder="请输入1-推荐，0-不推荐" />
+        <el-form-item label="标签1"
+                      prop="tagId1">
+          <el-input v-model="form.tagId1"
+                    placeholder="请输入标签id1" />
         </el-form-item>
-        <el-form-item label="删除标志" prop="delFlag">
-          <el-input v-model="form.delFlag" placeholder="请输入删除标志" />
+        <el-form-item label="标签2"
+                      prop="tagId2">
+          <el-input v-model="form.tagId2"
+                    placeholder="请输入标签id2" />
+        </el-form-item>
+        <el-form-item label="标签3"
+                      prop="tagId3">
+          <el-input v-model="form.tagId3"
+                    placeholder="请输入标签id3" />
         </el-form-item>
       </el-form>
       <template #footer>
         <div class="dialog-footer">
-          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button type="primary"
+                     @click="submitForm">确 定</el-button>
           <el-button @click="cancel">取 消</el-button>
         </div>
       </template>
@@ -217,6 +237,16 @@ function getList() {
     culturalTopicContentList.value = response.rows;
     total.value = response.total;
     loading.value = false;
+  });
+}
+
+// 更改开关
+function handleSwitchChange(row) {
+  console.log("row:", row);
+  updateCulturalTopicContent(row).then((response) => {
+    proxy.$modal.msgSuccess("修改成功");
+    open.value = false;
+    getList();
   });
 }
 
@@ -309,12 +339,12 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除文化专题内容编号为"' + _ids + '"的数据项？').then(function() {
+  proxy.$modal.confirm('是否确认删除文化专题内容编号为"' + _ids + '"的数据项？').then(function () {
     return delCulturalTopicContent(_ids);
   }).then(() => {
     getList();
     proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
+  }).catch(() => { });
 }
 
 /** 导出按钮操作 */
